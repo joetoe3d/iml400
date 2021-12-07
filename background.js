c89bd952b9c721df52e540685a6150986bacb877
joetoe3d <jftolent@usc.edu>
2021-12-07 14:33:03 -0800

    s

--- background.js
+++ background.js
@@ -74,58 +74,77 @@ function draw() {
   centerX += (mouseX - centerX) * 0.01;
   centerY += (mouseY - centerY) * 0.01;
 
-  // calculate new points
-  for (var i = 0; i < formResolution; i++) {
-    x[i] += random(-stepSize, stepSize);
-    y[i] += random(-stepSize, stepSize);
-    // uncomment the following line to show position of the agents
-    // ellipse(x[i] + centerX, y[i] + centerY, 5, 5);
-  }
+    // init shape
+    centerX = width / 2;
+    centerY = height / 2;
+    var angle = radians(360 / formResolution);
+    for (var i = 0; i < formResolution; i++) {
+        x.push(cos(angle * i) * initRadius);
+        y.push(sin(angle * i) * initRadius);
+    }
 
-  if (filled) {
-    fill(random(100));
-  } else {
-    noFill();
-  }
+    stroke(255);
+    strokeWeight(0.75);
+    background(0);
+}
 
-  beginShape();
-  // first controlpoint
-  curveVertex(x[formResolution - 1] + centerX, y[formResolution - 1] + centerY);
+function draw() {
+    // floating towards mouse position
+    centerX += (mouseX - centerX) * 0.01;
+    centerY += (mouseY - centerY) * 0.01;
 
-  // only these points are drawn
-  for (var i = 0; i < formResolution; i++) {
-    curveVertex(x[i] + centerX, y[i] + centerY);
-  }
-  curveVertex(x[0] + centerX, y[0] + centerY);
+    // calculate new points
+    for (var i = 0; i < formResolution; i++) {
+        x[i] += random(-stepSize, stepSize);
+        y[i] += random(-stepSize, stepSize);
+        // uncomment the following line to show position of the agents
+        // ellipse(x[i] + centerX, y[i] + centerY, 5, 5);
+    }
 
-  // end controlpoint
-  curveVertex(x[1] + centerX, y[1] + centerY);
-  endShape();
+    if (filled) {
+        fill(random(100));
+    } else {
+        noFill();
+    }
+
+    beginShape();
+    // first controlpoint
+    curveVertex(x[formResolution - 1] + centerX, y[formResolution - 1] + centerY);
+
+    // only these points are drawn
+    for (var i = 0; i < formResolution; i++) {
+        curveVertex(x[i] + centerX, y[i] + centerY);
+    }
+    curveVertex(x[0] + centerX, y[0] + centerY);
+
+    // end controlpoint
+    curveVertex(x[1] + centerX, y[1] + centerY);
+    endShape();
 }
 
 function mousePressed() {
-  // init shape on mouse position
-  centerX = mouseX;
-  centerY = mouseY;
-  var angle = radians(360 / formResolution);
-  var radius = initRadius * random(0.5, 1);
-  for (var i = 0; i < formResolution; i++) {
-    x[i] = cos(angle * i) * initRadius;
-    y[i] = sin(angle * i) * initRadius;
-  }
+    // init shape on mouse position
+    centerX = mouseX;
+    centerY = mouseY;
+    var angle = radians(360 / formResolution);
+    var radius = initRadius * random(0.5, 1);
+    for (var i = 0; i < formResolution; i++) {
+        x[i] = cos(angle * i) * initRadius;
+        y[i] = sin(angle * i) * initRadius;
+    }
 }
 
 function keyReleased() {
-  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
-  if (keyCode == DELETE || keyCode == BACKSPACE) background(0);
-  if (key == '1') filled = false;
-  if (key == '2') filled = true;
+    if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+    if (keyCode == DELETE || keyCode == BACKSPACE) background(255);
+    if (key == '1') filled = false;
+    if (key == '2') filled = true;
 
-  // pauze/play draw loop
-  if (key == 'f' || key == 'F') freeze = !freeze;
-  if (freeze) {
-    noLoop();
-  } else {
-    loop();
-  }
+    // pauze/play draw loop
+    if (key == 'f' || key == 'F') freeze = !freeze;
+    if (freeze) {
+        noLoop();
+    } else {
+        loop();
+    }
 }
