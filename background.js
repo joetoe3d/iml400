6ae6daf33b15f64b3a87f42beb91f30c619fc3db
joetoe3d <jftolent@usc.edu>
2021-12-07 13:37:16 -0800

    e

--- background.js
+++ background.js
@@ -44,9 +44,16 @@ var y = [];
 var filled = false;
 var freeze = false;
 
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    }
+
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.parent("bg");
+    canvas.position(0, 0);
+    canvas.style('z-index', '-1');
+
 
   // init shape
   centerX = width / 2;
