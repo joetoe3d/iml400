881ebf5025a9e558d175092c9eec46f345a6020b
joetoe3d <jftolent@usc.edu>
2021-12-07 13:30:44 -0800

    e

--- background.js
+++ background.js
@@ -45,88 +45,80 @@ var filled = false;
 var freeze = false;
 
 function setup() {
-    
+    var myCanvas = createCanvas(windowWidth, windowHeight);
+    myCanvas.parent("bg");
 
-        var clientHeight = document.getElementById('about').clientHeight;
-        var clientWidth = document.getElementById('about').clientWidth;
+  // init shape
+  centerX = width / 2;
+  centerY = height / 2;
+  var angle = radians(random(1,360) / formResolution);
+  for (var i = 0; i < formResolution; i++) {
+    x.push(cos(angle * i) * initRadius);
+    y.push(sin(angle * i) * initRadius);
+  }
 
-        var cnv = createCanvas(clientWidth, clientHeight);
-        cnv.parent("about");
-        background(255);
-    
-   
-
-    // init shape
-    centerX = width / 2;
-    centerY = height / 2;
-    var angle = radians(random(1, 360) / formResolution);
-    for (var i = 0; i < formResolution; i++) {
-        x.push(cos(angle * i) * initRadius);
-        y.push(sin(angle * i) * initRadius);
-    }
-
-    stroke(0, 50);
-    strokeWeight(0.75);
-
+  stroke(0, 50);
+  strokeWeight(0.75);
+  background(255);
 }
 
 function draw() {
-    // floating towards mouse position
-    centerX += (mouseX - centerX) * 0.01;
-    centerY += (mouseY - centerY) * 0.01;
+  // floating towards mouse position
+  centerX += (mouseX - centerX) * 0.01;
+  centerY += (mouseY - centerY) * 0.01;
 
-    // calculate new points
-    for (var i = 0; i < formResolution; i++) {
-        x[i] += random(-stepSize, stepSize);
-        y[i] += random(-stepSize, stepSize);
-        // uncomment the following line to show position of the agents
-        // ellipse(x[i] + centerX, y[i] + centerY, 5, 5);
-    }
+  // calculate new points
+  for (var i = 0; i < formResolution; i++) {
+    x[i] += random(-stepSize, stepSize);
+    y[i] += random(-stepSize, stepSize);
+    // uncomment the following line to show position of the agents
+    // ellipse(x[i] + centerX, y[i] + centerY, 5, 5);
+  }
 
-    if (filled) {
-        fill(random(255));
-    } else {
-        noFill();
-    }
+  if (filled) {
+    fill(random(255));
+  } else {
+    noFill();
+  }
 
-    beginShape();
-    // first controlpoint
-    curveVertex(x[formResolution - 1] + centerX, y[formResolution - 1] + centerY);
+  beginShape();
+  // first controlpoint
+  curveVertex(x[formResolution - 1] + centerX, y[formResolution - 1] + centerY);
 
-    // only these points are drawn
-    for (var i = 0; i < formResolution; i++) {
-        curveVertex(x[i] + centerX, y[i] + centerY);
-    }
-    curveVertex(x[0] + centerX, y[0] + centerY);
+  // only these points are drawn
+  for (var i = 0; i < formResolution; i++) {
+    curveVertex(x[i] + centerX, y[i] + centerY);
+  }
+  curveVertex(x[0] + centerX, y[0] + centerY);
 
-    // end controlpoint
-    curveVertex(x[1] + centerX, y[1] + centerY);
-    endShape();
+  // end controlpoint
+  curveVertex(x[1] + centerX, y[1] + centerY);
+  endShape();
 }
 
 function mousePressed() {
-    // init shape on mouse position
-    centerX = mouseX;
-    centerY = mouseY;
-    var angle = radians(360 / formResolution);
-    var radius = initRadius * random(0.5, 1);
-    for (var i = 0; i < formResolution; i++) {
-        x[i] = cos(angle * i) * initRadius;
-        y[i] = sin(angle * i) * initRadius;
-    }
+  // init shape on mouse position
+  centerX = mouseX;
+  centerY = mouseY;
+  var angle = radians(360 / formResolution);
+  var radius = initRadius * random(0.5, 1);
+  for (var i = 0; i < formResolution; i++) {
+    x[i] = cos(angle * i) * initRadius;
+    y[i] = sin(angle * i) * initRadius;
+  }
 }
 
 function keyReleased() {
-    if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
-    if (keyCode == DELETE || keyCode == BACKSPACE) background(255);
-    if (key == '1') filled = false;
-    if (key == '2') filled = true;
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+  if (keyCode == DELETE || keyCode == BACKSPACE) background(255);
+  if (key == '1') filled = false;
+  if (key == '2') filled = true;
 
-    // pauze/play draw loop
-    if (key == 'f' || key == 'F') freeze = !freeze;
-    if (freeze) {
-        noLoop();
-    } else {
-        loop();
-    }
+  // pauze/play draw loop
+  if (key == 'f' || key == 'F') freeze = !freeze;
+  if (freeze) {
+    noLoop();
+  } else {
+    loop();
+  }
 }
